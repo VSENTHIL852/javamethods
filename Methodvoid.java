d7939b92eed103488e90236ea9ef1a4c0f7207cc
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-03-17 21:21:22 +0530

    Create Methodvoid.java

---
+++ Methodvoid.java
@@ -0,0 +1,16 @@
+public class Methodvoid{
+    public static void main(String[] args){
+        greeting();
+        greeting();
+        greeting();
+        greeting();
+        greeting();
+        greeting();
+        
+
+
+    }
+    static void greeting(){
+        System.out.println("Hello world!!");
+    }
+}
